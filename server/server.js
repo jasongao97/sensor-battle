dbf8d62ed046279aadc464f4513bcb744262a430
jasongao97 <jasongao97@gmail.com>
2020-12-05 00:19:47 +0800

    player-car mapping

--- server/server.js
+++ server/server.js
@@ -6,6 +6,28 @@
 const PORT = process.env.PORT || 3000;
 const UDP_PORT = 33333;
 
+// Cars
+const STOP_TIME = 200;
+const LOCAL_HOST = "127.0.0.1";
+
+const cars = [
+  {
+    port: 58001,
+    timeout: null,
+    player: null,
+  },
+  {
+    port: 58002,
+    timeout: null,
+    player: null,
+  },
+  {
+    port: 58003,
+    timeout: null,
+    player: null,
+  },
+];
+
 // For live web app
 const express = require("express");
 const app = express();
@@ -47,7 +69,9 @@ udpserver.on("message", (msg, { address, port }) => {
   const message = decoder.decode(msg).replace(/[\r\n]/, "");
   if (!message) return;
 
-  address += `:${port}`
+  executeCommand(message);
+
+  address += `:${port}`;
   console.log(`${address} sends: '${message}'`);
   io.emit("message", { message, address });
 });
@@ -58,3 +82,31 @@ udpserver.on("listening", () => {
 });
 
 udpserver.bind(UDP_PORT);
+
+function executeCommand(command) {
+  const format = /^(g|j|m|r|s)\.(go|back|left|right|fire)$/;
+  if (!format.test(command)) return;
+
+  const [player, action] = command.split(".");
+
+  for (i in cars) {
+    if ((cars[i].player = player)) {
+      const { port, timeout } = cars[i];
+      clearTimeout(timeout);
+
+      if (action === "go") {
+        udpserver.send("w", port, LOCAL_HOST);
+      } else if (action === "back") {
+        udpserver.send("s", port, LOCAL_HOST);
+      } else if (action === "left") {
+        udpserver.send("a", port, LOCAL_HOST);
+      } else if (action === "right") {
+        udpserver.send("d", port, LOCAL_HOST);
+      }
+
+      cars[i].timeout = setTimeout(() => {
+        udpserver.send("0", port, LOCAL_HOST);
+      }, STOP_TIME);
+    }
+  }
+}
